21dcddfd4196dd8f81691a9a0b860b4b2cf567be
Gunarto Septiawan <gunartosept@gmail.com>
2021-10-11 16:04:36 +0700

    tugas_17_javascript
    Tugas Niomic

---
+++ tugas17.js
@@ -0,0 +1,30 @@
+function Ubah(){
+  var huruf = "Saya beLajar bahaSa peMrograman deNgan khUsuk."
+  var huruf2 = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual."
+
+  console.log(huruf.toLowerCase());
+  console.log(huruf.toUpperCase());
+  console.log(huruf2.toLowerCase());
+  console.log(huruf2.toUpperCase());
+}
+
+Ubah();
+
+function Kode(){
+  var kode = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+
+  console.log(kode.charCodeAt(0));
+  console.log(kode.charCodeAt(1));
+  console.log(kode.charCodeAt(2));
+  console.log(kode.charCodeAt(3));
+  console.log(kode.charCodeAt(4));
+  console.log(kode.charCodeAt(5));
+
+}
+
+Kode();
+
+
+
+
+
